243355cef436ac457d6657e75048fd92438f1162
luketudor <Luke.Tudor@myob.com>
2017-11-28 11:55:03 +1300

    4 tests done

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+            var splitString = input.Split(',', '\n');
+            return splitString.Sum(int.Parse);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorShould.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+        [Test]
+        public void ReturnEmptyString()
+        {
+            var stringCalculator = new Calculator();
+            var actual = stringCalculator.Add("");
+            const int expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnSingleInputAsOutput()
+        {
+            var stringCalculator = new Calculator();
+            var actual = stringCalculator.Add("1");
+            const int expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnTwoInputSum()
+        {
+            var stringCalculator = new Calculator();
+            var actual = stringCalculator.Add("1,2");
+            const int expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnMoreThanTwoInputSum()
+        {
+            var stringCalculator = new Calculator();
+            var actual = stringCalculator.Add("1,2,1,1,1,1,1,1,1");
+            const int expected = 10;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnSumForMultipleSeparatorInput()
+        {
+            var stringCalculator = new Calculator();
+            var actual = stringCalculator.Add("1\n2,1,1,1,1,1,1,1");
+            const int expected = 10;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
